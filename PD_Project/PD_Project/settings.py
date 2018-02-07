19e9942c0f9b849680faf3492b1358360e7fb579
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2018-02-06 18:18:42 -0600

    Change the setting so that it works with the secret json file.

--- PD_Project/PD_Project/settings.py
+++ PD_Project/PD_Project/settings.py
@@ -9,35 +9,62 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import json
 import os
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Get all the information needed for independent development from secrets.json
+_secret = os.path.join(BASE_DIR, "secrets.json")
+with open(_secret) as f:
+    _secrets = json.loads(f.read())
+
+
+def get_secret(_setting, _secret=_secrets):
+    """Returns an specific setting from the secrets.json file"""
+    try:
+        return _secrets[_setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(_setting)
+        raise ImproperlyConfigured(error_msg)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1q&^y3qk#$7jvn0p=#_knnja#jd=y26s!6_@9i9!j!w_bs@i_1'
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if get_secret("DEBUG") == 1:
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = get_secret("ALLOWED_HOSTS")
 
 # Application definition
 
-INSTALLED_APPS = [
+CORE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
+
+THIRD_PARTY_APPS = [
+    'rest_framework'
+]
+
+PROJECT_APPS = [
+    'rest_api.apps.RestApiConfig'
+]
+
+INSTALLED_APPS = CORE_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,8 +74,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rest_framework',
-    'rest_api'
 ]
 
 ROOT_URLCONF = 'PD_Project.urls'
@@ -76,10 +101,14 @@ WSGI_APPLICATION = 'PD_Project.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': get_secret("DB_NAME"),
+            'USER': get_secret("DB_USER"),
+            'PASSWORD': get_secret("DB_PASSWORD"),
+            'HOST': get_secret("DB_HOST"),
+            'PORT': get_secret("DB_PORT"),
+        }
 }
 
 
