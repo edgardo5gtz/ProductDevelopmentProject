7b85e571779875e1476262bb077ccd848d029aea
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2018-02-06 22:18:39 -0600

    Create models and make migrations

--- PD_Project/apps/Risks/models.py
+++ PD_Project/apps/Risks/models.py
@@ -1,50 +1,22 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 
-#Insurers
 class Risk(models.Model):
     """Custom RiskType"""
-    name = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name = _('risk')
-        verbose_name_plural = _('risks')
-        db_table = 'risks'
-
-    def __str__(self):
-        """ Return name """
-        return self.name
+    name = models.CharField(max_length=50, blank=True, null=True)
+    pass
 
 
 class RiskType(models.Model):
     """Template for modeling a particular type of risk"""
-    risk = models.ForeignKey('Risk')
+    risk = models.ForeignKey(Risk, on_delete=models.CASCADE)
     type = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name = _('risk type')
-        verbose_name_plural = _('risk types')
-        db_table = 'risk types'
-
-    def __str__(self):
-        """ Return name """
-        return self.type
 
 
 class RiskField(models.Model):
     """Data inputs associated with a particular RiskType"""
     risk_type = models.ManyToManyField('RiskType')
     field = models.CharField(max_length=100)
-
-    class Meta:
-        verbose_name = _('risk field')
-        verbose_name_plural = _('risk field')
-        db_table = 'risk fields'
-
-    def __str__(self):
-        """ Return name """
-        return self.field
 
 
 class FieldDateVal(models.Model):
@@ -60,7 +32,7 @@ class FieldEnumVal(models.Model):
         ('B', 'choice_B')
     )
     risk_field = models.ManyToManyField('RiskField')
-    value = models.DateField()
+    value = models.CharField(choices=ENUM_OPTIONS, max_length=200)
 
 
 class FieldTextVal(models.Model):
@@ -73,3 +45,4 @@ class FieldNumberVal(models.Model):
     """Number field value associated with a RiskField"""
     risk_field = models.ManyToManyField('RiskField')
     value = models.IntegerField()
+
