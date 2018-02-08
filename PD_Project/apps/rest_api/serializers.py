963d1f034f2c1bd8b3decfc5d0561ccfc5bd6a96
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2018-02-07 18:45:49 -0600

    Add serializers for the rest of the models.

--- PD_Project/apps/rest_api/serializers.py
+++ PD_Project/apps/rest_api/serializers.py
@@ -1,10 +1,52 @@
 from rest_framework import serializers
-from ..Risks.models import Risk
+from ..Risks.models import *
 
 
 class RiskSerializer(serializers.ModelSerializer):
-    """ Return risk model serialized data"""
+    """ Return Risk model serialized data"""
     class Meta:
         model = Risk
+        fields = '__all__'
+
+
+class RiskTypeSerializer(serializers.ModelSerializer):
+    """ Return RiskType model serialized data"""
+    class Meta:
+        model = RiskType
+        fields = '__all__'
+
+
+class RiskFieldSerializer(serializers.ModelSerializer):
+    """ Return RiskField model serialized data"""
+    class Meta:
+        model = RiskField
+        fields = '__all__'
+
+
+class TextFieldSerializer(serializers.ModelSerializer):
+    """ Return TextFieldVal model serialized data"""
+    class Meta:
+        model = TextFieldVal
+        fields = '__all__'
+
+
+class EnumFieldSerializer(serializers.ModelSerializer):
+    """ Return EnumFieldVal model serialized data"""
+    class Meta:
+        model = EnumFieldVal
+        fields = '__all__'
+
+
+class DateFieldSerializer(serializers.ModelSerializer):
+    """ Return DateFieldVal model serialized data"""
+    class Meta:
+        model = DateFieldVal
+        fields = '__all__'
+
+
+class NumberFieldSerializer(serializers.ModelSerializer):
+    """ Return NumberFieldVal model serialized data"""
+    class Meta:
+        model = NumberFieldVal
         fields = '__all__'
 
