dfea080e160f7ad70152556107ee150c62f4f880
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2018-02-08 18:44:35 -0600

    Create card template to show api calls.
    Api calls currently fetching risks.

--- PD_Project/static/main.js
+++ PD_Project/static/main.js
@@ -1,10 +1,23 @@
-
-
-new Vue({
+let vm = new Vue({
     el: '#app',
     delimiters: ['${','}'],
     data: {
-        message: 'Hello Vue!',
-        msg: "Helooo"
+        risks: ""
+    },
+    methods: {
+        getRisksList:  () => {
+            axios.get('/risks_api/risks/')
+                .then((response) => {
+                    this.error = false;
+                    vm.risks = response.data;
+                    console.log(response.data);
+                })
+                .catch((error) => {
+                    this.error = true;
+                    this.risks = "Ups";
+                    console.log(error);
+                });
+        }
     }
 });
+
