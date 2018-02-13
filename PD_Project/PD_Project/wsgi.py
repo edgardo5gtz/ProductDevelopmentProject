1ab54591cdde56509fbbfdff7197e5a1e6016ab8
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2018-02-12 23:03:16 -0600

    Add project path to wsgi

--- PD_Project/PD_Project/wsgi.py
+++ PD_Project/PD_Project/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/home/Escritorio/BriteCore/ProductDevelopmentProject/PD_Project')
+sys.path.append('/home/Escritorio/BriteCore/ProductDevelopmentProject/PD_Project/PD_Project')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "./settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PD_Project.settings")
 
 application = get_wsgi_application()
